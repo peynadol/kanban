a1b7e6313ec4452ed98949c417cb93b580f7cf1f
peynadol <peynadol@gmail.com>
2025-06-04 11:49:52 +0100

    implement drag and drop

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,8 @@
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
+//TODO: style the dragged task
+//TODO: consider using DragOverlay component in dnd-kit
+//TODO: figure out how to re-order tasks within the same column, bumping other tasks up or down
 
 const Task = ({ task }) => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
